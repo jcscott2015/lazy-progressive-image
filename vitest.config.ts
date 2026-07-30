ae8cfe9770f7b8a004cbb693202e3a73065f9d0f
John C. Scott <jcscott@scottcomm.com>
2026-07-30 11:57:29 -0700

    Fix Vitest trying to run the browser-only test file

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "happy-dom",
+    exclude: ["src/**/*.browser.test.ts", "src/**/*.browser.test.tsx"],
     include: [
       "src/**/*.test.ts",
       "src/**/*.test.tsx",
